958617bd8097fad124b6729b7ba849e988e4f757
nawarSoumaya <nawar.soumaya@gmail.com>
2015-03-22 15:37:58 +0100

    Creation de la classe connexion

--- src/tn/Hozier/nawar/gestionBanque/entity/Test.java
+++ src/tn/Hozier/nawar/gestionBanque/entity/Test.java
@@ -1,15 +1,11 @@
 package tn.Hozier.nawar.gestionBanque.entity;
 
+import tn.Hozier.nawar.gestionBanque.utile.Cnx;
+
 public class Test {
 
 	public static void main(String[] args) {
-		Client moi = new Client(01,"soumaya");
-		Compte c = new CompteCourant(01, 215,moi);
-		Compte c2 = new CompteEpargne(01, 326,moi);
-		
-		moi.affiche();
-		//bonjour
-			
+	 Cnx.getInstance();
 
 	}
 
